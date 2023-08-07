593fb5036c09c276908c71f8673d905b5cc058fc
Y2Kelly <georgedotkelly@gmail.com>
2023-08-07 16:37:17 +1200

    This is my final commit for Project 1

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -22,7 +22,7 @@ if(squareX > 100) {
 }
 
 function setup_wallpaper(pWallpaper) {
-  pWallpaper.output_mode(DEVELOP_GLYPH);
+  pWallpaper.output_mode(GRID_WALLPAPER);
   pWallpaper.resolution(FIT_TO_SCREEN);
   pWallpaper.show_guide(false); //set this to false when you're ready to print
 
